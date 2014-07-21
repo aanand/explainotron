86801cedf30ae65d65461cb06f5e61cdb2cce186
Aanand Prasad <aanand.prasad@gmail.com>
2014-07-20 22:53:22 -0700

    Tests

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,9 @@ WORKDIR /code
 ADD requirements.txt /code/requirements.txt
 RUN pip install -r requirements.txt
 
+ADD requirements-dev.txt /code/requirements-dev.txt
+RUN pip install -r requirements-dev.txt
+
 ADD . /code
 
 ENV PYTHONUNBUFFERED 1
---
+++ tests/explainer_test.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+from unittest import TestCase
+
+from extensions import explainer
+
+
+class ExplainerTest(TestCase):
+    def test_basic(self):
+        tweets = [
+            'explaining politics to my dad',
+            'how to begin to explain my love \u2026',
+        ]
+        objects = [o for t in tweets for o in explainer.get_objects(t, explainer.Explainer.verbs)]
+        self.assertEqual(list(objects), [
+            'politics',
+            'my love \u2026',
+        ])
+
+    def test_pronoun_ending(self):
+        tweet = 'i cannot explain how beautiful he is'
+        objects = explainer.get_objects(tweet, ['explain'])
+        self.assertEqual(list(objects), [])
+
+    def test_i(self):
+        tweet = 'i cannot explain how beautiful i am'
+        objects = explainer.get_objects(tweet, ['explain'])
+        self.assertEqual(list(objects), [])
